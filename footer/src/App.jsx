f82c80558b8b2d252afdbf714b63c23ba3455418
4823 <meet4823@gmail.com>
2022-03-21 15:22:02 +0530

    footer done

--- footer/src/App.jsx
+++ footer/src/App.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-
+import Foot from './component/Footer';
 const App = () => (
-  <div className="container">
-    <div>Name: footer</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Empty CSS</div>
-  </div>
+<div>
+<Foot/>
+</div>
+ 
 );
 ReactDOM.render(<App />, document.getElementById("app"));
--- footer/webpack.config.js
+++ footer/webpack.config.js
@@ -44,7 +44,11 @@ module.exports = {
       name: "footer",
       filename: "remoteEntry.js",
       remotes: {},
-      exposes: {},
+      exposes: {
+
+        
+        "./Footer": "./src/component/Footer.js",
+      },
       shared: {
         ...deps,
         react: {
