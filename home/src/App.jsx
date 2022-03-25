b87fd30f71f42c7b065b8c3624bdcb3379a21c36
Chetan1103 <chetan@costacloud.com>
2022-03-25 12:24:53 +0530

    updated

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Footer from 'footer/Footer';
-import Navbar from 'header/Navbar';
+//import Navbar from 'header/Navbar';
 import PhotoPage from 'content/PhotoPage'
-//import Navbar from "../../header/src/components/Navbar";
 
 const App = () => (
   <div className="container">
-    <Navbar/>
+    
     <PhotoPage/>
     <Footer/>
   </div>
